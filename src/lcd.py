2fd5cda00a2178c76a50aebe7c54fc15a916cf33
mrsp <stpiperakis@gmail.com>
2022-02-24 23:07:50 +0200

    initial commit

---
+++ src/lcd.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+ * LCD - Legged Contact Detection
+ *
+ * Copyright 2022-2023 Stylianos Piperakis and Michalis Maravgakis, Foundation for Research and Technology Hellas (FORTH)
+ * License: BSD
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Foundation for Research and Technology Hellas (FORTH) 
+ *	 nor the names of its contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+'''
+
+from sklearn import mixture
+from mlp import mlp
+import pickle as pickle
+from tensorflow.keras.models import load_model
+import os
+
+
+class lcd():
+    def __init__(self):
+        self.init = False
+	
+    def setConfiguration(self,  robot_, humanoid_, load_model_ = False, out_path = "."):
+        self.robot = robot_
+        self.humanoid = humanoid_
+        if(self.humanoid):
+            self.input_dim = 12  #F/T + IMU
+        else: 
+            self.input_dim = 9   #Force + IMU
+
+        if(load_model_):
+            self.mlp = mlp()
+            self.mlp.setDimReduction(self.input_dim)
+            self.mlp = load_model(out_path  +'/'+self.robot + '_MLP',compile=False)
+            self.model_log =  pickle.load(open(out_path  +'/'+self.robot + '_MLP_LOG', 'rb'))
+            #self.gmm = pickle.load(open(out_path+'/'+self.robot + '_gmm.sav', 'rb'))
+            self.init = True
+        else:
+            self.mlp = mlp()
+            self.mlp.setDimReduction(self.input_dim)
+            #self.gmm = mixture.GaussianMixture(n_components=3, covariance_type='full', max_iter=200, tol=5.0e-4, init_params = 'kmeans',warm_start=False, n_init=50, verbose=1)
+          
+
+    def fit(self,data_train, data_validation, save_model_ = False, data_labels = None, data_validation_labels = None):
+        print("Data Size ",data_train.size)
+        self.fitMLP(data_train,data_labels,data_validation,data_validation_labels, save_model_)
+        #print("Clustering with Gaussian Mixture Models")
+        #self.clusterGMM(save_model_)
+        self.init = True
+
+
+    def predict(self, data_):
+        leg_probabilities = self.mlp.predict(data_.reshape(1,-1))
+        #gait_phase = self.gmm.predict(leg_probabilities)
+        #gait_phase_proba = self.gmm.predict_proba(leg_probabilities)
+        return leg_probabilities #gait_phase, gait_phase_proba, 
+
+
+    def predict_dataset(self, data_):
+        leg_probabilities = self.mlp.predict(data_)
+        #gait_phase = self.gmm.predict(leg_probabilities)
+
+        return leg_probabilities #,gait_phase
+
+  
+
+    def reduceMLP(self,data_train,data_labels,data_validation,data_validation_labels, save_model_):
+        self.mlp.fit(data_train,data_labels,data_validation, data_validation_labels, 300, 16)
+        self.leg_probabilities =  self.mlp.model.predict(data_train)
+        self.model_log = self.mlp.model_log.history
+        if(save_model_):
+            self.mlp.model.save(self.robot + '_MLP')
+            with open(self.robot + '_MLP_LOG', 'wb') as file_pi:
+                pickle.dump(self.mlp.model_log, file_pi)
+
+    def clusterGMM(self, save_model_):
+        self.gmm.fit(self.leg_probabilities)
+        if(save_model_):
+            with open(self.robot + '_gmm.sav', 'wb') as file:
+                pickle.dump(self.gmm, file)
+        self.gait_phases = self.gmm.predict(self.leg_probabilities)
---
+++ src/mlp.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+ * LCD - Legged Contact Detection
+ *
+ * Copyright 2022-2023 Stylianos Piperakis and Michalis Maravgakis, Foundation for Research and Technology Hellas (FORTH)
+ * License: BSD
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Foundation for Research and Technology Hellas (FORTH) 
+ *	 nor the names of its contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+'''
+
+
+
+
+from tensorflow.keras.models import Sequential, Model,load_model, save_model
+from tensorflow.keras.layers import  Dense, Dropout, Flatten, MaxPooling1D
+import tensorflow as tf
+import tempfile
+import os
+from tensorflow.keras.utils import get_custom_objects
+from tensorflow.keras.layers import Activation
+# Hotfix function
+def make_keras_picklable():
+    def __getstate__(self):
+        model_str = ""
+        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=False, dir=os.getcwd()) as fd:
+            save_model(self, fd.name, overwrite=True)
+            model_str = fd.read()
+        d = {'model_str': model_str}
+        os.unlink(fd.name)
+        return d
+
+    def __setstate__(self, state):
+        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=False, dir=os.getcwd()) as fd:
+            fd.write(state['model_str'])
+            fd.flush()
+            model = load_model(fd.name)
+        os.unlink(fd.name)
+        self.__dict__ = model.__dict__
+
+
+    cls = Model
+    cls.__getstate__ = __getstate__
+    cls.__setstate__ = __setstate__
+
+class mlp():
+    def __init__(self):
+        self.init = False
+        make_keras_picklable()
+
+
+    def setArchitecture(self, input_dim):
+        self.model = keras.Sequential()
+        # Input
+        self.model.add(Dense(320, activation='relu',input_shape=(input_dim, )))
+        # First hidden
+        self.model.add(Dense(272, activation='relu'))
+        # Dropout
+        self.model.add(Dropout(0.3))
+        # Second hidden
+        self.model.add(Dense(160, activation='relu'))
+        # Max pooling
+        self.model.add(MaxPooling1D(pool_size=2, name="MaxPooling1D"))
+        self.model.add(Flatten())
+        self.model.add(Dense(304, activation='relu'))
+        self.model.add(Dense(2, activation='softmax'))
+        self.model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy'])
+        self.init = True
+
+
+    def fit(self, x_train, y_train, x_validation, y_validation, epochs_, batch_size_):
+        self.model_log = self.model.fit(x_train, ytrain, validation_data=(x_validation,  y_validation), epochs=epochs_, batch_size=batch_size_,  verbose=1, shuffle=True)
